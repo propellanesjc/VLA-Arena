2b7b4cd9837284d2efa6e7dc839529f65829fada
Jiachen Shen <2769649101@qq.com>
2025-12-31 19:47:54 +0800

    fix: fix init_file bugs (#11)

--- vla_arena/vla_arena/envs/__init__.py
+++ vla_arena/vla_arena/envs/__init__.py
@@ -18,5 +18,5 @@ from .bddl_base_domain import TASK_MAPPING
 from .problems import *
 from .robots import *
 
-# from .env_wrapper import OffScreenRenderEnv, SegmentationRenderEnv
+from .env_wrapper import OffScreenRenderEnv, SegmentationRenderEnv
 from .venv import DummyVectorEnv, SubprocVectorEnv
